a23032e17ebe4a770d4a0e6201ba866ce2782ad7
Antonio Simões <simoeslealneto@gmail.com>
2020-11-10 00:18:26 -0300

    installmed vuetify and vuex-persist libs

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <v-app> </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-</style>
+<script>
+export default {
+  name: "App",
+
+  components: {},
+
+  data: () => ({
+    //
+  }),
+};
+</script>
